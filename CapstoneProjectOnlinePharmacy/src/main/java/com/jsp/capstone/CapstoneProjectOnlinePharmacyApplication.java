7d1683fc0f0ff5e3fa413cee995829bda42cba8f
Saisree658 <gorentlasaisree9@gmail.com>
2024-12-29 19:19:28 +0530

    Add files via upload

---
+++ CapstoneProjectOnlinePharmacy/src/main/java/com/jsp/capstone/CapstoneProjectOnlinePharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectOnlinePharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectOnlinePharmacyApplication.class, args);
+	}
+
+}
---
+++ CapstoneProjectOnlinePharmacy/src/test/java/com/jsp/capstone/CapstoneProjectOnlinePharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectOnlinePharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
